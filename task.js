7e1a0ad4d802d193600f4dd1a41e7863389e17c2
Semyon Zarajsky <zarajskysam92@gmail.com>
2020-08-16 12:57:51 +0300

    corrected

--- task.js
+++ task.js
@@ -1,21 +1,18 @@
 const input = document.getElementsByClassName("tasks__input");
 const list = document.getElementById("tasks__list");
+const applyButton = document.getElementById("tasks__add");
 let tasks = list.children;
 
 input[0].addEventListener("keydown", (event) =>{
     if (event.keyCode == 13) {
         event.preventDefault();
-        if (input[0].value) {
-            list.innerHTML += 
-                `<div class="task">
-                    <div class="task__title">
-                        ${input[0].value}
-                    </div>
-                    <a href="#" class="task__remove">&times;</a>
-                </div>`;
-            input[0].value = "";
-        }
+        addTask();
     }
+})
+
+applyButton.addEventListener("click", (event) =>{
+    event.preventDefault();
+    addTask();
 })
 
 list.addEventListener("click", (event) =>{
@@ -23,6 +20,19 @@ list.addEventListener("click", (event) =>{
         event.target.closest(".task").remove();
     }
 })
+
+function addTask() {
+    if (input[0].value) {
+        list.innerHTML += 
+            `<div class="task">
+                <div class="task__title">
+                    ${input[0].value}
+                </div>
+                <a href="#" class="task__remove">&times;</a>
+            </div>`;
+        input[0].value = "";
+    }
+}
 
 
 
